9ab3ab666064d377699d50f12ef21d96b492c8b8
Ashton Omdahl <aomdahl@gmail.com>
2018-03-31 21:26:53 -0600

    Updated bad file paths

--- Validation_scripts/run_reverse_comparison_on_unfiltered.sh
+++ Validation_scripts/run_reverse_comparison_on_unfiltered.sh
@@ -4,16 +4,16 @@
 #Here I only show some of the steps for convenience
 #Extract the relevant files and condense them to the relevant directories using internal_motif_analysis.py
 #This version, made on 3/29, was run on the unfiltered data (not chosen by conservation or anything)
-mkdir nofilter_MA-MA_comparison_results
+mkdir -p nofilter_MA-MA_comparison_results
 python3 internal_motif_analysis.py /mnt/d/AshtonData/MPI_all_data/SummaryFiles_8-17/ma.12.29.TF.superTable.remade.tsv /mnt/d/AshtonData/MPI_all_data/SummaryFiles_8-17/MA_results/ /mnt/d/AshtonData/MPI_all_data/SummaryFiles_8-17/nofilter_MA-MA_comparison_results/
 
-mkdir nofilter_MA-RNA_comparison_results
+mkdir -p nofilter_MA-RNA_comparison_results
 python3 internal_motif_analysis.py /mnt/d/AshtonData/MPI_all_data/SummaryFiles_8-17/ma.12.29.TF.superTable.remade.tsv /mnt/d/AshtonData/MPI_all_data/SummaryFiles_8-17/RNASeq_results/ /mnt/d/AshtonData/MPI_all_data/SummaryFiles_8-17/nofilter_MA-RNA_comparison_results/
 
-mkdir nofilter_RNA-RNA_comparison_results
+mkdir -p nofilter_RNA-RNA_comparison_results
 python3 internal_motif_analysis.py /mnt/d/AshtonData/MPI_all_data/SummaryFiles_8-17/RNA.8.17.TF.superTable.tsv /mnt/d/AshtonData/MPI_all_data/SummaryFiles_8-17/RNASeq_results/ /mnt/d/AshtonData/MPI_all_data/SummaryFiles_8-17/nofilter_RNA-RNA_comparison_results/
 
-mkdir nofilter_RNA-MA_comparison_results
+mkdir -p nofilter_RNA-MA_comparison_results
 python3 internal_motif_analysis.py /mnt/d/AshtonData/MPI_all_data/SummaryFiles_8-17/RNA.8.17.TF.superTable.tsv /mnt/d/AshtonData/MPI_all_data/SummaryFiles_8-17/MA_results/ /mnt/d/AshtonData/MPI_all_data/SummaryFiles_8-17/nofilter_RNA-MA_comparison_results/
 
 
@@ -34,9 +34,9 @@ bash motif_comparison.sh /mnt/d/AshtonData/MPI_all_data/SummaryFiles_8-17/nofilt
 
 
 #Now, we want to condense the results into a readable table. Here I use the Homer html parser I built over the summer:
-mkdir nofilter_results
-python3 HomerTools.py --dirs /mnt/d/AshtonData/MPI_all_data/SummaryFiles_8-17/nofilter_MA-MA_comparison_results/ > /mnt/d/AshtonData/MPI_all_data/SummaryFiles_8-17/nofilter_results/MA-MA-comparison_summary.tsv &
-python3 HomerTools.py --dirs /mnt/d/AshtonData/MPI_all_data/SummaryFiles_8-17/nofilter_RNA-RNA_comparison_results/ > /mnt/d/AshtonData/MPI_all_data/SummaryFiles_8-17/nofilter_results/RNA-RNA-comparison_summary.tsv &
-python3 HomerTools.py --dirs /mnt/d/AshtonData/MPI_all_data/SummaryFiles_8-17/nofilter_MA-RNA_comparison_results/ > /mnt/d/AshtonData/MPI_all_data/SummaryFiles_8-17/nofilter_results/MA-RNA-comparison_summary.tsv &
-python3 HomerTools.py --dirs /mnt/d/AshtonData/MPI_all_data/SummaryFiles_8-17/nofilter_RNA-MA_comparison_results/ > /mnt/d/AshtonData/MPI_all_data/SummaryFiles_8-17/nofilter_results/RNA-MA-comparison_summary.tsv &
+mkdir -p nofilter_results
+python3 ../Toolkit_scripts/HomerTools.py --dirs /mnt/d/AshtonData/MPI_all_data/SummaryFiles_8-17/nofilter_MA-MA_comparison_results/ > /mnt/d/AshtonData/MPI_all_data/SummaryFiles_8-17/nofilter_results/MA-MA-comparison_summary.tsv &
+python3 ../Toolkit_scripts/HomerTools.py --dirs /mnt/d/AshtonData/MPI_all_data/SummaryFiles_8-17/nofilter_RNA-RNA_comparison_results/ > /mnt/d/AshtonData/MPI_all_data/SummaryFiles_8-17/nofilter_results/RNA-RNA-comparison_summary.tsv &
+python3 ../Toolkit_scripts/HomerTools.py --dirs /mnt/d/AshtonData/MPI_all_data/SummaryFiles_8-17/nofilter_MA-RNA_comparison_results/ > /mnt/d/AshtonData/MPI_all_data/SummaryFiles_8-17/nofilter_results/MA-RNA-comparison_summary.tsv &
+python3 ../Toolkit_scripts/HomerTools.py --dirs /mnt/d/AshtonData/MPI_all_data/SummaryFiles_8-17/nofilter_RNA-MA_comparison_results/ > /mnt/d/AshtonData/MPI_all_data/SummaryFiles_8-17/nofilter_results/RNA-MA-comparison_summary.tsv &
 
